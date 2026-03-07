e8e7581ff45c9adfaedc26a52aaad02f983d48d9
nurai223 <nuraizhoomartkyzy@gmail.com>
2026-03-07 14:49:32 +0600

    I did my part

--- mysite/store/admin.py
+++ mysite/store/admin.py
@@ -1,3 +1,63 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+class ReviewInline(admin.TabularInline):
+    model = Review
+    extra = 1
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline, ReviewInline]
+
+
+class SubcategoryInline(admin.TabularInline):
+    model = SubCategory
+    extra = 1
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [SubcategoryInline]
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 1
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    inlines = [CartItemInline]
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemInline]
+
+class ChatMessageInline(admin.TabularInline):
+    model = ChatMessage
+    extra = 1
+
+
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    inlines = [ChatMessageInline]
+
+
+admin.site.register(UserProfile)
+admin.site.register(PromoCode)
+admin.site.register(Payment)
+admin.site.register(Delivery)
+admin.site.register(ReturnRequest)
+admin.site.register(SellerPayout)
+admin.site.register(Favorite)
+admin.site.register(Address)
+admin.site.register(Notification)
