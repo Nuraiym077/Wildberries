133915587e9cb44cf216af0948db1b7a525482bd
daiyr2005 <daiyrkanatbekov40@gmail.com>
2026-03-11 03:05:51 +0600

    run

--- mysite/store/models.py
+++ mysite/store/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from phonenumber_field.modelfields import PhoneNumberField
+from django.utils import timezone
 
 ROLE_CHOICES = (
     ('buyer', 'buyer'),
